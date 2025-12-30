6419feb9b0984b608a1b36687cf359ad618d6596
shi1523 <connect.shivanikumari@gmail.com>
2025-12-30 12:13:28 +0530

    Create TaskModal.js

---
+++ TaskModal.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import Button from './Button';
+import Input from './Input';
+
+const TaskModal = ({ task, onClose, onSave }) => {
+};
+
+export default TaskModal;
