5ca6ea4eac494ffeb1c078d812d9cebf2574b20a
shi1523 <connect.shivanikumari@gmail.com>
2025-12-30 12:12:00 +0530

    Create LoginForm.js

---
+++ LoginForm.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { Mail, Lock } from 'lucide-react';
+import { useAuth } from '../context/AuthContext';
+import Alert from './Alert';
+import Input from './Input';
+import Button from './Button';
+
+const LoginForm = ({ onToggle }) => {
+};
+
+export default LoginForm;
