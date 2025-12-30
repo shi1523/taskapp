4d7fc0d013a1d7984eccb7d75c2501a2a8aa770e
shi1523 <connect.shivanikumari@gmail.com>
2025-12-30 12:15:43 +0530

    Create AuthPage.js

---
+++ AuthPage.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+
+const AuthPage = () => {
+};
+
+export default AuthPage;
