e20219c1943b92c1ff70493ba8d9a4ba3f7c4dda
shi1523 <connect.shivanikumari@gmail.com>
2025-12-30 12:14:25 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { Plus, Edit2, Trash2, Search, User, LogOut } from 'lucide-react';
+import { useAuth } from '../context/AuthContext';
+import api from '../api/api';
+import TaskModal from './TaskModal';
+
+const Dashboard = () => {
+};
+
+export default Dashboard;
